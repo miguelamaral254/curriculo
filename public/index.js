8aedcaa86c4dec491b14f3fa7bfd688d630e49f9
miguelamaral254 <118134009+miguelamaral254@users.noreply.github.com>
2023-01-18 21:24:01 -0300

    att

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ function menuShow(){
     let menuMobile = document.querySelector('.nav-mobile-item')
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
+        document.querySelector('.icon').src="../Imagens/close-menu-burguer.png"
     }else{
         menuMobile.classList.add('open')
     }
